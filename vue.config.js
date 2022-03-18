55ee3ef5247a989df43ea2e9efbc2cd7996cfd3b
川 <chc82347794@163.com>
2022-03-18 10:19:22 +0800

    系统初始布局

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+/**
+ * @name: vue.config
+ * @author: chuanchuan
+ * @date: 2022-03-17 19:35
+ * @description：vue.config
+ * @update: 2022-03-17 19:35
+ */
+
+module.exports = {
+  css: {
+    loaderOptions: {
+      less: {
+        javascriptEnabled: true,
+      }
+    }
+  }
+}
